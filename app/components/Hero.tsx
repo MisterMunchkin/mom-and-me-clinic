08792cf9d8d28cc3c72e9bfa66fd5d14fc487fe5
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-06-13 11:45:36 +0800

    col for hero buttons for mobile. - updated padding on main

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,10 +10,10 @@ export default function Hero() {
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">How can we help you today?</h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">With over 30 years of experience between </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Book appoinment</a>
-            <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Find a doctor</a>
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">All services <span aria-hidden="true">→</span></a>
+          <div className="mt-10 flex flex-col gap-y-3 md:flex-row md:gap-x-6 items-center justify-center">
+            <a href="#" className="rounded-md bg-indigo-600 w-40 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Book appoinment</a>
+            <a href="#" className="rounded-md bg-indigo-600 w-40 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Find a doctor</a>
+            <a href="#" className="text-sm font-semibold w-40 px-3.5 leading-6 text-gray-900">All services <span aria-hidden="true">→</span></a>
           </div>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between pt-24 md:p-24">
         <Hero />
       </main>
     </>
