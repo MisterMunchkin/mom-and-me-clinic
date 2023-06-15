3ce6d8ad5988c731cc9fb16e81a7036178fb6421
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-06-15 10:13:07 +0800

    moved hero outside of main as main should be the doctors list

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,14 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <main className="flex min-h-screen flex-col items-center justify-between pt-24 md:px-24 md:pt-0">
+      <div className="min-h-screen pt-24 md:px-24 md:pt-8">
         <Hero />
+      </div>
+      <main id="main-content" className="flex flex-col items-center justify-between">
+        {/* Main info should be list of doctors where you can book */}
+        <div style={{minHeight: 2000}}>
+          Test
+        </div>
       </main>
     </>
   )
