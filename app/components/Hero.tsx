e806287202b9658973d7e8ca6e407637e5c9517e
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-06-15 09:59:13 +0800

    removed stuff

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,11 +2,11 @@ export default function Hero() {
   return (
     <div className="relative isolate px-6 pt-14 lg:px-8">
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+        {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
             Announcing our next round of funding. <a href="#" className="font-semibold text-primary"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
           </div>
-        </div>
+        </div> */}
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">How can we help you today?</h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">With over 30 years of experience between </p>
