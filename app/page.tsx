1bc3579e6c60262023753fd6a2f893e6b081af3e
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-08-27 14:44:43 +0800

    added obgyn banner for services

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Maps from '@/components/Maps';
 import NavLayout from '@/components/nav-layout/NavLayout';
 import { Suspense } from 'react';
 import LoadingServices from './loading-services';
+import OvariesIcon from '@/public/lifesavers_ovaries.png'
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -27,11 +29,29 @@ export default function Home() {
         </section>
 
         <section id="services" className="pt-24 md:pt-16">
-          <div className="mx-auto pb-6 pl-6 md:pb-12">
+          <div className="mx-auto pb-4 pl-6 md:pb-6">
             <div className="text-left">
               <h1 className="text-2xl font-bold tracking-tight text-gray-650 sm:text-4xl">Our Services</h1>
             </div>
           </div>
+
+          <div className="w-screen md:w-full md:rounded-lg h-[6rem] bg-pastel-green flex place-items-center justify-center mb-6">
+            <Image 
+              className='mr-6'
+              src={OvariesIcon}
+              alt='Ovaries'
+              width={85}
+            />
+            <div className="px-2">
+              <h3 className="text-xl tracking-light text-gray-650 sm:text-2xl">
+                Obstetrics & Gynecology
+              </h3>
+              <h4 className="text-lg sm:text-xl text-gray-650 text-center">
+                OBG-YN
+              </h4>
+            </div>
+          </div>
+
           <Suspense fallback={<LoadingServices />}>
             <Services />
           </Suspense>
