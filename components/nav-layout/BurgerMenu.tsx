570487a487104434aea18b8bf48cddf00e09e6cd
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-09-04 10:29:09 +0800

    fixed navigation and scrollbehavior on mobile and desktop

--- components/nav-layout/BurgerMenu.tsx
+++ components/nav-layout/BurgerMenu.tsx
@@ -1,21 +1,36 @@
-import { Drawer, IconButton, List, ListItem, ListItemPrefix } from "@/utilities/material-tailwind-export";
+import { Drawer, IconButton, List, ListItem } from "@/utilities/material-tailwind-export";
 import React from "react";
-import { XMarkIcon, Square3Stack3DIcon } from "@heroicons/react/20/solid";
-import { useRouter } from "next/navigation";
+import { XMarkIcon } from "@heroicons/react/20/solid";
 
 interface BurgerMenuProps {
   showDrawer: boolean;
   setShowDrawer: (show: boolean) => void;
 }
 
+const navItems = [
+  {
+    description: 'Home',
+    link: '#hero'
+  },
+  {
+    description: "Book Appointment",
+    link: '/appointment'
+  },
+  {
+    description: 'Browse Services',
+    link: '#services'
+  },
+  {
+    description: 'Our Doctors',
+    link: '#doctors'
+  },
+  {
+    description: 'Location',
+    link: '#location'
+  }
+]
+
 export default function BurgerMenu({showDrawer, setShowDrawer}: BurgerMenuProps) {
-  const router = useRouter();
-
-  const navigate = (nav: string): void => {
-    router.push(nav);
-    setShowDrawer(false);
-  } 
-
   return (
     <React.Fragment>
       <Drawer
@@ -38,56 +53,16 @@ export default function BurgerMenu({showDrawer, setShowDrawer}: BurgerMenuProps)
           </IconButton>
         </div>
         <List>
-          <ListItem
-            onClick={() => navigate('#hero')}
-          >
-            <ListItemPrefix>
-              <Square3Stack3DIcon 
-                className="text-gray-650 h-6 w-6"
-              />
-            </ListItemPrefix>
-            Home
-          </ListItem>
-          <ListItem
-            onClick={() => navigate('/appointment')}
-          >
-            <ListItemPrefix>
-              <Square3Stack3DIcon 
-                className="text-gray-650 h-6 w-6"
-              />
-            </ListItemPrefix>
-            Book Appointment
-          </ListItem>
-          <ListItem
-            onClick={() => navigate('#services')}
-          >
-            <ListItemPrefix>
-              <Square3Stack3DIcon 
-                className="text-gray-650 h-6 w-6"
-              />
-            </ListItemPrefix>
-            Browse Services
-          </ListItem>
-          <ListItem
-            onClick={() => navigate('#doctors')}
-          >
-            <ListItemPrefix>
-              <Square3Stack3DIcon 
-                className="text-gray-650 h-6 w-6"
-              />
-            </ListItemPrefix>
-            Our Doctors
-          </ListItem>
-          <ListItem
-            onClick={() => navigate('#location')}
-          >
-            <ListItemPrefix>
-              <Square3Stack3DIcon 
-                className="text-gray-650 h-6 w-6"
-              />
-            </ListItemPrefix>
-            Location
-          </ListItem>
+          {navItems.map((navItem,index) => (
+            <a
+              key={index}
+              href={navItem.link}
+            >
+              <ListItem>
+                {navItem.description}
+              </ListItem>
+            </a>
+          ))}
         </List>
       </Drawer>
     </React.Fragment>
