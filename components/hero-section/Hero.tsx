a82b3e9e6cec806f394955fa0148b73d87e258c7
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-09-05 23:41:18 +0800

    added under construction icons to book appointment

--- components/hero-section/Hero.tsx
+++ components/hero-section/Hero.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import PlantIcon from "@/public/lifesavers_plant_5.png"
 import { CalendarDaysIcon } from "@heroicons/react/20/solid";
 import React from "react";
+import UnderconstructionIcon from '@/public/icons/under_construction.png';
 
 export default function Hero() {
   return (
@@ -24,18 +25,36 @@ export default function Hero() {
           </Typography>
 
           <div className="mt-10 flex space-x-4 md:space-x-16 items-center justify-center pt-6">
-            <Link 
-              href={{
-                pathname: `/appointment`
-              }} 
-              className="py-2 px-4 rounded-full bg-pastel-pink text-white-ivory hover:shadow-lg hover:shadow-pastel-pink/50">
-              <span
-                className="button-text items-center"
-              >
-                <CalendarDaysIcon className="w-5 h-5 inline mr-3" />
-                <span className="font-semibold tracking-wider text-sm">Book Appointment</span>
-              </span>
-            </Link>
+            <div
+              className="relative"
+            >
+              <Image
+                src={UnderconstructionIcon}
+                width={35}
+                height={35}
+                alt="under_construction_icon"
+                className="absolute bottom-0 left-0"
+              />
+              <Image
+                src={UnderconstructionIcon}
+                width={35}
+                height={35}
+                alt="under_construction_icon"
+                className="absolute bottom-0 right-0"
+              />
+              <Link 
+                href={{
+                  pathname: `/appointment`
+                }} 
+                className="py-2 px-4 rounded-full bg-pastel-pink text-white-ivory hover:shadow-lg hover:shadow-pastel-pink/50">
+                <span
+                  className="button-text items-center"
+                > 
+                  <CalendarDaysIcon className="w-5 h-5 inline mr-3" />
+                  <span className="font-semibold tracking-wider text-sm">Book Appointment</span>
+                </span>
+              </Link>
+            </div>
             <a 
               href="#services" 
               className="underline underline-offset-4 hover:text-pastel-pink cursor-pointer">
