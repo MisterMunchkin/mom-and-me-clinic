8f1653e4b678354c69262177456fa441a74785f9
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-09-08 22:40:25 +0800

    updated grid cols for services

--- components/services-section/Services.tsx
+++ components/services-section/Services.tsx
@@ -10,7 +10,7 @@ export default async function Services() {
   if (!services) return <div>No services fetched.</div>
 
   return (
-    <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
       {services.map((service) => (
         <Service key={service.name} service={service} />
       ))}
