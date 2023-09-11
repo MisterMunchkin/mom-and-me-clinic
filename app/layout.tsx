3b24059ae05606e4f5a3303efa20df52807c0ff7
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-09-11 10:02:14 +0800

    added new metadata tags (#51)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
+import { Metadata } from 'next'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Mom & Me Clinic',
-  description: 'Obstetrics and Gynecology services in Room 611 Cebu Velez Medical Arts F. Ramos St., Cebu City',
+  description: 'Women, Kids and Teens Health Hub. Located in Room 611 Cebu Velez Medical Arts F. Ramos St., Cebu City',
+  openGraph: {
+    type: 'website',
+    url: 'https://momandme.clinic',
+    title: 'Mom & Me Clinic',
+    description: 'Women, Kids and Teens Health Hub.',
+    siteName: 'Mom & Me Clinic',
+    images: [{
+      url: 'https://momandme.clinic/og_image.png'
+    }]
+  }
 }
 
 export default function RootLayout({
