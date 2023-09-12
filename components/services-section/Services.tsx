cb40fe6d38f586dd17b7370acf4b04b0411baca4
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-09-12 23:59:48 +0800

    fixed ui

--- components/services-section/Services.tsx
+++ components/services-section/Services.tsx
@@ -10,7 +10,7 @@ export default async function Services() {
   if (!services) return <div>No services fetched.</div>
 
   return (
-    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+    <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 items-start justify-stretch">
       {services.map((service) => (
         <Service key={service.name} service={service} />
       ))}
