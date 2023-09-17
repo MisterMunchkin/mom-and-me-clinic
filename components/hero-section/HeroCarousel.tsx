e8e77ae1b449a17e9775a2788f5761a93bcf1649
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-09-17 18:37:50 +0800

    updated Image component on carousel to use sizes, and placeholder

--- components/hero-section/HeroCarousel.tsx
+++ components/hero-section/HeroCarousel.tsx
@@ -13,7 +13,7 @@ import Carousel3 from "@/public/carousel_3.jpeg";
 export default function HeroCarousel() {
   return (
     <Swiper 
-      className="w-full h-full overflow-x-hidden md:rounded-lg md:w-8/12"
+      className="w-full h-[300px] overflow-x-hidden md:rounded-lg md:w-8/12 md:h-[600px]"
       modules={[Autoplay]}
       loop={true}
       // pagination={{
@@ -31,6 +31,9 @@ export default function HeroCarousel() {
           src={Carousel1}
           alt="Image 1"
           className="h-full w-full object-cover"
+          placeholder='blur'
+          sizes='(max-width: 720px) 100vw, 40vw'
+          fill
         />
       </SwiperSlide>
       <SwiperSlide>
@@ -38,6 +41,9 @@ export default function HeroCarousel() {
           src={Carousel2}
           alt="Image 2"
           className="h-full w-full object-cover"
+          placeholder='blur'
+          sizes='(max-width: 720px) 100vw, 40vw'
+          fill
         />
       </SwiperSlide>
       <SwiperSlide>
@@ -45,6 +51,9 @@ export default function HeroCarousel() {
           src={Carousel3}
           alt="Image 3"
           className="h-full w-full object-cover"
+          placeholder='blur'
+          sizes='(max-width: 720px) 100vw, 40vw'
+          fill
         />
       </SwiperSlide>
     </Swiper>
