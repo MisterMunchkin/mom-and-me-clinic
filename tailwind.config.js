8d7f78fd7864c9fe31f784e2752f290359f1d495
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-09-18 12:46:03 +0800

    32 need new design for services card in main page (#54)
    * added accordion shadcn ui
    
    * added card shadcn component
    
    * moved json back to root
    
    * fixed ui
    
    * fixed tailwind config
    
    * tailwindcss-animate upgrade
    
    * using popover for service and updated UI
    
    * melon instead of pastel pink for the popover on Service

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,10 +35,25 @@ module.exports = withMT({
       },
       transitionProperty: {
         'top': 'top'
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    require('tailwindcss-animate')
   ],
 });
