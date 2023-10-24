19074e9e7757a5700b2e25bdef75f1d11e1335e5
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-10-24 11:48:06 +0800

    removed coming soon locks removed

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
 
   // once appointment is ready, we should create a checker to make sure that booking confirmed
   // is only accessed when appointment is truly created, maybe we can check the booking ID.
-  if (request.nextUrl.pathname.startsWith('/appointment/booking-confirmed')) {
-    return NextResponse.rewrite(new URL('/appointment-coming-soon', request.url));
-  }
+  // if (request.nextUrl.pathname.startsWith('/appointment/booking-confirmed')) {
+  //   return NextResponse.rewrite(new URL('/appointment-coming-soon', request.url));
+  // }
 }
